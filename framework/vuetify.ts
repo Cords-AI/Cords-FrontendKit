e26f64f261ce4bb09f9458cc1f340f3811f89aa0
Jon Harding <jharding@ubriety.com>
2022-11-21 11:44:14 -0500

    Add material icons support

--- framework/vuetify.ts
+++ framework/vuetify.ts
@@ -1,6 +1,8 @@
 import 'vuetify/styles';
 import 'vuetify/dist/vuetify.css';
 import '@mdi/font/css/materialdesignicons.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import { aliases, md } from 'vuetify/iconsets/md';
 
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
@@ -9,4 +11,11 @@ import * as directives from 'vuetify/directives';
 export default createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
 });
