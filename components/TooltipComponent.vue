db9aa2885e3b7e4866032edb944193d4bb76a575
Alex Cordy <acordy@ubriety.com>
2023-05-04 12:24:30 -0400

    Add TooltipComponent

---
+++ components/TooltipComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <QTooltip v-bind="$attrs"
+            ref="qRef"
+            transition-show="fade"
+            transition-hide="fade"
+            class="bg-primary"
+            :delay="500"
+            :offset="[10, 10]"
+  >
+    <slot></slot>
+  </QTooltip>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const qRef = ref();
+</script>
+
+<style lang="scss">
+[data-css-scope=quasar] .q-tooltip--style {
+  font-weight: var(--typography-strong-font-weight);
+}
+</style>
